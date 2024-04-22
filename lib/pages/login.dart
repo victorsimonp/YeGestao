7d0b74c99cdc19934b4654ca3944151dbc53d979
unknown <victorsimon0406@gmail.com>
2024-04-22 14:46:27 -0300

    Facebook adicionado

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class login extends StatefulWidget {
   const login({super.key});
@@ -21,11 +21,11 @@ class _loginState extends State<login> {
             "YE Gestão de Saúde",
             style: TextStyle(fontSize: 20),
           ),
-          backgroundColor: Color.fromARGB(208, 202, 201, 201),
+          backgroundColor: Color.fromRGBO(71, 146, 121, 0.612),
           centerTitle: true,
         ),
         resizeToAvoidBottomInset: false,
-        backgroundColor: Color.fromARGB(255, 207, 206, 206),
+        backgroundColor: Color.fromRGBO(182, 249, 234, 0.855),
         body: SingleChildScrollView(
             child: Center(
                 child: Padding(
@@ -113,7 +113,9 @@ class _loginState extends State<login> {
                                       ))
                                 ],
                               )),
-                              SizedBox(height: 30,),
+                          SizedBox(
+                            height: 30,
+                          ),
                           SizedBox(
                             width: double.infinity,
                             child: Padding(
@@ -122,8 +124,8 @@ class _loginState extends State<login> {
                               child: ElevatedButton(
                                 onPressed: () {},
                                 style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStateProperty.all(Colors.grey),
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Color.fromRGBO(71, 146, 121, 0.612)),
                                 ),
                                 child: Text(
                                   (queroEntrar) ? "Entrar" : "Cadastrar",
@@ -139,7 +141,9 @@ class _loginState extends State<login> {
                             padding:
                                 const EdgeInsets.only(right: 10.0, left: 10.0),
                             child: ElevatedButton(
-                              onPressed: () {_logarComGoogle();},
+                              onPressed: () {
+                                _logarComGoogle();
+                              },
                               style: ButtonStyle(
                                 backgroundColor: MaterialStateProperty.all(
                                     Color.fromARGB(255, 240, 82, 82)),
@@ -161,6 +165,37 @@ class _loginState extends State<login> {
                                 ],
                               ),
                             ),
+                          ),
+                          SizedBox(height: 2),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 10.0, left: 10.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                _logarComFacebook();
+                              },
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.blue)),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.facebook, color: Colors.white),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    "Entrar com o Facebook",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
                           ),
                           TextButton(
                               onPressed: () {
@@ -191,10 +226,7 @@ class FirebaseAuthService {
       return credential.user;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'email-already-in-use') {
-       
-      } else {
-        
-      }
+      } else {}
       return null;
     }
   }
@@ -207,42 +239,51 @@ class FirebaseAuthService {
       return credential.user;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found' || e.code == 'wrong-password') {
-        
-      } else {
-        
-      }
+      } else {}
       return null;
     }
   }
 }
 
 Future<void> _logarComGoogle() async {
-    final GoogleSignIn _logarComGoogle = GoogleSignIn();
+  final GoogleSignIn _logarComGoogle = GoogleSignIn();
 
-    try {
-      final GoogleSignInAccount? googleSignInAccount =
-          await _logarComGoogle.signIn();
+  try {
+    final GoogleSignInAccount? googleSignInAccount =
+        await _logarComGoogle.signIn();
 
-      if (googleSignInAccount != null) {
-        final GoogleSignInAuthentication googleSignInAuthentication =
-            await googleSignInAccount.authentication;
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
 
-        final AuthCredential credential = GoogleAuthProvider.credential(
-            idToken: googleSignInAuthentication.idToken,
-            accessToken: googleSignInAuthentication.accessToken);
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
 
-        // final UserCredential userCredential =
-        //     await _fi.signInWithCredential(credential);
+      // final UserCredential userCredential =
+      //     await _fi.signInWithCredential(credential);
 
-        // // Navegue para a próxima tela após o login com sucesso
-        // if (userCredential.user != null) {
-        //   Navigator.pushNamed(context as BuildContext, "/home");
-        // }
-      }
-    } catch (e) {
-      
+      // // Navegue para a próxima tela após o login com sucesso
+      // if (userCredential.user != null) {
+      //   Navigator.pushNamed(context as BuildContext, "/home");
+      // }
     }
+  } catch (e) {}
+}
+
+Future<void> _logarComFacebook() async {
+  try {
+    final LoginResult result = await FacebookAuth.instance.login();
+
+    if (result.status == LoginStatus.success) {
+      // Login com Facebook bem-sucedido, você pode prosseguir com o seu fluxo de autenticação aqui
+      print('Login com Facebook bem-sucedido!');
+    } else {
+      // Login com Facebook falhou, trate de acordo
+      print('Login com Facebook falhou: ${result.message}');
+    }
+  } catch (e) {
+    // Trate quaisquer erros de exceção aqui
+    print('Erro ao tentar fazer login com Facebook: $e');
   }
-
-    
-
+}
